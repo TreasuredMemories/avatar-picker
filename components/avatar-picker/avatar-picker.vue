6b5d9c3952e654fdf18c649632675c7499f42667
zhaozihao <zhaozihao@1fda.com>
2023-04-21 10:41:19 +0800

    [Initial](skeleton)

---
+++ components/avatar-picker/avatar-picker.vue
@@ -0,0 +1,24 @@
+<!-- created by Zack 2023-0201 -->
+<template>
+	<view></view>
+</template>
+
+<script>
+	export default{
+		name: 'avatar-picker',
+		mounted() {
+			console.log('picker was mounted')
+		},
+		data() {
+			return {
+				
+			}
+		},
+		methods:{
+			
+		}
+	}
+</script>
+
+<style>
+</style>
